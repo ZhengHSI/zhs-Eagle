9bf055e8674483e8370b57f78fb0ac6bb65859b6
root <zhenghsi@qq.com>
2025-05-28 14:25:36 +0800

    fix

---
+++ generate_data/generate_data.sh
@@ -0,0 +1,2 @@
+python allocation.py --outdir /workdir/datasets/ShareGPT_Vicuna_unfiltered --dataset /workdir/huggingface.co/datasets/Aeala/ShareGPT_Vicuna_unfiltered
+python allocation.py --outdir /workdir/datasets/ultrachat_200k --dataset /workdir/huggingface.co/datasets/HuggingFaceH4/ultrachat_200k
